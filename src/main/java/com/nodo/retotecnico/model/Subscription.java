894cb60ba17fac136c8f1efdd676e74ac28e6efa
Angela Peña <angela.pena05@uptc.edu.co>
2026-07-23 13:02:39 -0500

    fix: corrección de archivos y persmiso de acceso de url de front

--- src/main/java/com/nodo/retotecnico/model/Subscription.java
+++ src/main/java/com/nodo/retotecnico/model/Subscription.java
@@ -1,6 +1,7 @@
 package com.nodo.retotecnico.model;
 
 import java.util.Date;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,8 +9,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
@@ -38,16 +37,9 @@ public class Subscription {
     @Column(name = "subscription_type")
     private SubscriptionType subscriptionType;
 
-    private String platform;
+    //private String platform;
 
     private String country;
-
-    @ManyToOne
-    @JoinColumn(name = "expansion_pack_id")
-    private ExpansionPack expansionPack;
-
-    @Column(columnDefinition = "TEXT")
-    private String comments;
 
     @Column(name = "consent_marketing")
     private Boolean consentMarketing;
--- src/main/java/com/nodo/retotecnico/model/SubscriptionType.java
+++ src/main/java/com/nodo/retotecnico/model/SubscriptionType.java
@@ -1,7 +1,6 @@
 package com.nodo.retotecnico.model;
 
 public enum SubscriptionType {
-    NEWSLETTER,
     BETA_TESTING,
     FOCUS_GROUP,
     SIMMER_CHALLENGE
